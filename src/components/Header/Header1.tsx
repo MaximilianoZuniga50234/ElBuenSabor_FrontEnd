69aa04dcbba5e08b8890b96d58f255224c387f82
FJFFH <franciscofioreh@gmail.com>
2023-05-04 07:45:17 -0300

    cambios Header1

--- src/components/Header/Header1.tsx
+++ src/components/Header/Header1.tsx
@@ -7,8 +7,13 @@ const Header1 = () => {
       <div>
         <div className="imagen">
           <div className="separa">
-            <p>Tentate con nuestos mejores platos</p>
-            <button>Ver productos</button>
+            <div className="textoDiv">
+                <p className="texto m-0">Tentate con nuestos</p>
+                <p className="texto m-0">mejores platos</p>
+            </div>
+            <div className="botonDiv">
+                <button className="boton btn-lg">Ver productos</button>
+            </div>
           </div>
         </div>
       </div>
