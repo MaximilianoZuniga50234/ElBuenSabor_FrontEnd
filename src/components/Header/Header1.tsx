b6423bcaf4644381651a40aa9cbd4c86f389e33b
FJFFH <franciscofioreh@gmail.com>
2023-05-04 06:40:45 -0300

    crear Header1 Y eliminar HeaderUsuario

---
+++ src/components/Header/Header1.tsx
@@ -0,0 +1,19 @@
+import "./Header1.css";
+import { Link } from "react-router-dom";
+
+const Header1 = () => {
+  return (
+    <>
+      <div>
+        <div className="imagen">
+          <div className="separa">
+            <p>Tentate con nuestos mejores platos</p>
+            <button>Ver productos</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header1;
