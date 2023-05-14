7105d591b16538cd50fc8c606d3049843237dae8
Lautaro Jofré <lautyjofre1@gmail.com>
2023-05-14 13:35:51 -0300

    agrego audience

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     authorizationParams={{
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       redirect_uri: window.location.origin,
     }}
   >
