849f32735f60cbe34453338d5bed29dfa6e397a6
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2023-09-18 22:05:18 -0300

    hot fix delete userDTO

--- src/app/dto/UserDTO.ts
+++
@@ -1,9 +0,0 @@
-import { Base } from "../interfaces/Base";
-
-export interface UserDTO extends Base {
-  email: string;
-  fullName: string;
-  telephone: string;
-  role: string;
-  numberOfOrders: number;
-}
