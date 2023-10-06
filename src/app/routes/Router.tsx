a10c168e80039bd3172a3ed36fb93c1a4e65f0b1
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2023-10-06 20:16:54 -0300

    hot fix

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import Landing from "../pages/Landing";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
 import About from "../pages/About";
@@ -9,19 +8,24 @@ import Profile from "../pages/profile/Profile";
 import Categories from "../pages/categories/Categories";
 import CallbackPage from "../components/auth0/CallbackPage";
 import { AuthenticationGuard } from "../components/auth0/AuthenticationGuard";
+import NotFound from "../pages/NotFound";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="*" element={<Error />} />
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<AuthenticationGuard component={About} />} />
+      <Route
+        path="/about"
+        element={<AuthenticationGuard component={About} />}
+      />
       <Route path="/employees" element={<Employees />} />
       <Route path="/customers" element={<Customers />} />
       <Route path="/profile/:id" element={<Profile />} />
       <Route path="/categories" element={<Categories />} />
       <Route path="/callback" element={<CallbackPage />} />
+      <Route path="/error" element={<Error />} />
     </Routes>
   );
 };
