d10754daa4d798bc56b68b854778c39af1c95c75
Lautaro Jofré <lautyjofre1@gmail.com>
2023-10-17 19:31:44 -0300

    Cambios en la página de redirección al login

--- src/app/components/auth0/AuthenticationGuard.tsx
+++ src/app/components/auth0/AuthenticationGuard.tsx
@@ -1,5 +1,5 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
-
+import "./authenticationGuard.css"
 type Props = {
     component: React.ComponentType<object>;
 };
@@ -7,9 +7,16 @@ type Props = {
 export const AuthenticationGuard = ({ component }: Props) => {
     const Component = withAuthenticationRequired(component, {
         onRedirecting: () => (
-            <div>
-                <div>Redireccionando...</div>
-            </div>
+
+            <div className="redirecter__container">
+                <div className="redirecter__spinner" >
+                    <div className="redirecter__item redirecter__item-1"></div>
+                    <div className="redirecter__item redirecter__item-2"></div>
+                    <div className="redirecter__item redirecter__item-3"></div>
+                    <div className="redirecter__item redirecter__item-4"></div>
+                </div >
+                <h4 className="redirecter__h4">Cargando...</h4>
+            </div >
         ),
     });
 
