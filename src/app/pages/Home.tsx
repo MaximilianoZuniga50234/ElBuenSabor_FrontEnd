8362e84e05622918268deac495d535f714e7920a
CandeZuniga <candela.zuniga@alumnos.frm.utn.edu.ar>
2023-10-17 19:22:28 -0300

    cambios de carruzel

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
-import Modal from "../components/modal/Modal"; 
+import Modal from "../components/modal/Modal";
 import { Product } from "../interfaces/Product";
 import "./Home.css";
 import { getAllProduct } from "../functions/ProductAPI";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import { BiCartAdd } from "react-icons/bi";
-import { GrNext, GrPrevious } from 'react-icons/gr';
 
 const Home = () => {
   const [products, setProducts] = useState<Product[]>([]);
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -25,19 +25,29 @@ const Home = () => {
     getProductsJSONFetch();
   }, []);
 
-  const moveCarousel = (direction: "prev" | "next") => {
-    const numProducts = products.length;
+  const ButtonClick = (product: Product) => {
 
-    if (direction === "prev") {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? numProducts - 1 : prevIndex - 1
-      );
-    } else if (direction === "next") {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === numProducts - 1 ? 0 : prevIndex + 1
-      );
-    }
   };
+
+  const items = products.map((product) => (
+    <div key={product.id} className="product-card">
+      <img
+        src={product.imgUrl}
+        alt={product.denomination}
+        className="product-image"
+        onClick={() => openModal(product)}
+      />
+      <div className="product-info">
+        <h3 className="product-title">{product.denomination}</h3>
+        <div className="product-price">
+          <h4>${product.salePrice}</h4>
+          <button className="button" onClick={() => ButtonClick(product)}>
+            <BiCartAdd className="cart-icon" />
+          </button>
+        </div>
+      </div>
+    </div>
+  ));
 
   const openModal = (product: Product) => {
     setSelectedProduct(product);
@@ -45,57 +55,22 @@ const Home = () => {
   };
 
   const closeModal = () => {
-    setSelectedProduct(null);
     setIsModalOpen(false);
   };
 
-  const ButtonClick = (product: Product) => {
-
-  };
-
   return (
-    <div>
-      <div style={{ height: "50px"}}></div>
-      <h1 style={{ textAlign: "center", margin: "0 0 20px 0" }}>Destacados</h1>
-      <div className="carousel-container">
-        <span onClick={() => moveCarousel("prev")} className="carousel-arrow">
-          <GrPrevious />
-        </span>
-        <div className="carousel-content">
-          <div
-            style={{
-              display: "flex",
-              maxHeight: "250px",
-              transform: `translateX(-${currentIndex * 125}px)`,
-              transition: "transform 0.3s ease-in-out",
-            }}
-          >
-            {products.map((product) => (
-              <div key={product.id} className="product-card">
-                <img
-                  src={product.imgUrl}
-                  alt={product.denomination}
-                  className="product-image"
-                  onClick={() => openModal(product)}
-                />
-                <div className="product-info">
-                  <h3 className="product-title">{product.denomination}</h3>
-                  <div className="product-price">
-                    <h4>${product.salePrice}</h4>
-                    <button className="button"onClick={() => ButtonClick(product)}>
-                      <BiCartAdd className="cart-icon" />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-        <span onClick={() => moveCarousel("next")} className="carousel-arrow">
-          <GrNext />
-        </span>
-      </div>
-
+    <div className="Home">
+      <h1>Destacados</h1>
+      <AliceCarousel
+        items={items}
+        responsive={{
+          0: { items: 1 },
+          568: { items: 2 },
+          1024: { items: 3 },
+        }}
+        infinite
+        mouseTracking
+      />
       <Modal
         isOpen={isModalOpen}
         product={selectedProduct}
@@ -107,4 +82,3 @@ const Home = () => {
 };
 
 export default Home;
-
