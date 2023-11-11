e824bb88568e603cc0b965aeba5d8631f3fc4a07
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2023-11-10 21:29:16 -0300

    Implemented category for routes

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,41 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
-import Error from "../pages/Error";
-import About from "../pages/About";
-import Employees from "../pages/employees/Employees";
-import Customers from "../pages/customers/Customers";
-import Profile from "../pages/profile/Profile";
-import CallbackPage from "../components/auth0/CallbackPage";
-import { AuthenticationGuard } from "../components/auth0/AuthenticationGuard";
 import NotFound from "../pages/NotFound";
-import AllProducts from "../pages/all products/AllProducts";
-import ItemProductABM from "../pages/abm/itemProduct/ItemProductABM";
-import Carrousel from "../components/home/carrousel/Carrousel";
-import ItemStockABM from "../pages/abm/itemStock/ItemStockABM";
-import MeasurementUnitABM  from "../pages/abm/measurementUnit/MeasurementUnitABM";
-import ShoppingCart from "../components/home/shoppingCart/ShoppingCart";
+import PublicRoutes from "./PublicRoutes";
+import EmployeeRoutes from "./EmployeeRoutes";
+import AdminRoutes from "./AdminRoutes";
+import Error from "../pages/Error";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
-      <Route path="/home" element={<Home />} />
-      <Route
-        path="/about"
-        element={<AuthenticationGuard component={About} />}
-      />
-      <Route path="/employees" element={<Employees />} />
-      <Route path="/customers" element={<Customers />} />
-      <Route path="/profile/:id" element={<Profile />} />
-      <Route path="/products" element={<AllProducts />} />
-      <Route path="/callback" element={<CallbackPage />} />
-      <Route path="/carrousel" element={<Carrousel />} />
+      <Route path="/u/*" element={<PublicRoutes />} />
+      <Route path="/e/*" element={<EmployeeRoutes />} />
+      <Route path="/a/*" element={<AdminRoutes />} />
       <Route path="/error" element={<Error />} />
-      <Route path="/itemProductABM" element={<AuthenticationGuard component={ItemProductABM} />} />
-      <Route path="/itemStockABM" element={<AuthenticationGuard component={ItemStockABM} />} />
-      <Route path="/measurementUnitABM" element={<AuthenticationGuard component={MeasurementUnitABM} />} />
-      <Route path="/cart" element={<ShoppingCart />} />
     </Routes>
   );
 };
