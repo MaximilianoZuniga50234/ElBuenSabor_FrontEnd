8421780abdf3cf3d5c31344d47b800a64394e8f3
Lautaro Jofré <lautyjofre1@gmail.com>
2023-12-27 12:52:16 -0300

    Inserto a la base de datos las personas y direcciones según la totalidad de usuarios

--- src/app/functions/PersonAPI.ts
+++ src/app/functions/PersonAPI.ts
@@ -1,3 +1,5 @@
+import { Person } from "../interfaces/Person";
+
 export async function getAllPerson() {
   return await fetch("http://localhost:9000/api/v1/person").then((r) =>
     r.json()
@@ -8,4 +10,15 @@ export async function getOnePerson(id: string) {
   return await fetch(`http://localhost:9000/api/v1/person/${id}`).then((r) =>
     r.json()
   );
+}
+
+export async function addPerson(person: Person, token: string) {
+  await fetch(`http://localhost:9000/api/v1/person`, {
+    method: "POST",
+    body: JSON.stringify(person),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  });
 }
