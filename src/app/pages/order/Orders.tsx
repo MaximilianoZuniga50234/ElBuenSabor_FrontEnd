2aba17dc3d0f471dd3020fcf27fae1bbc4821d90
Lautaro Jofré <lautyjofre1@gmail.com>
2024-01-14 17:14:48 -0300

    Agrego funcionalidad a la vista de pedidos

--- src/app/pages/order/Orders.tsx
+++ src/app/pages/order/Orders.tsx
@@ -1,6 +1,6 @@
 import { FaChevronDown, FaChevronUp, FaSearch } from "react-icons/fa";
 import Table from "../../components/orders/OrdersTable";
-import { FormEvent, MouseEvent, useState, useEffect } from "react";
+import { MouseEvent, useState, useEffect } from "react";
 import { PurchaseOrder } from "../../interfaces/PurchaseOrder";
 import { getAllPurchaseOrder } from "../../functions/PurchaseOrderAPI";
 import "./orders.css";
@@ -8,10 +8,12 @@ import "./orders.css";
 const Orders = () => {
   const [active, setActive] = useState<boolean>(false);
   const [filter, setFilter] = useState<string>("");
-  // const [search, setSearch] = useState<string>("");
   const [orders, setOrders] = useState<PurchaseOrder[]>([]);
-  const filterOrders =
-    filter === "" ? orders : orders.filter((o) => o.status.status === filter);
+  const [changeState, setChangeOrderState] = useState<boolean>(false);
+  // const filterOrders =
+  // filter === "" || filter === "Sin filtro" ? orders : orders.filter((o) => o?.status?.status === filter);
+  const [filterOrders, setFilterOrders] = useState<PurchaseOrder[]>([]);
+  const [idFilter, setIdFilter] = useState<number>(0)
 
   const handleDrop = () => {
     setActive(!active);
@@ -22,22 +24,39 @@ const Orders = () => {
     setActive(false);
   };
 
-  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSearch = () => {
+    setFilterOrders(idFilter != 0 ? orders.filter((o) => o.id === idFilter) : orders)
+  };
+
+  const getAllItems = async () => {
+    try {
+      const response = await getAllPurchaseOrder();
+      setOrders(response);
+    } catch (error) {
+      console.error("Error", error);
+    }
   };
 
   useEffect(() => {
-    const getAllItems = async () => {
-      try {
-        const response = await getAllPurchaseOrder();
-        setOrders(response);
-      } catch (error) {
-        console.error("Error", error);
-      }
-    };
-
     getAllItems();
   }, []);
+
+  useEffect(() => {
+    if (changeState === true) {
+      getAllItems();
+      setChangeOrderState(false)
+    }
+  }, [changeState]);
+
+
+
+  useEffect(() => {
+    if (filter === "" || filter === "Sin filtro") {
+      setFilterOrders(orders)
+    } else {
+      setFilterOrders(orders.filter((o) => o?.status?.status === filter))
+    }
+  }, [filter, orders]);
 
   return (
     <main className="main_employees_list">
@@ -55,10 +74,22 @@ const Orders = () => {
           <div className={`filter_order_list ${active && "active"}`}>
             <ul>
               <li
-                className={`${filter === "Por aceptar" ? "active" : ""}`}
+                className={`${filter === "Sin filtro" ? "active" : ""}`}
                 onClick={handleFilter}
               >
-                Por aceptar
+                Sin filtro
+              </li>
+              <li
+                className={`${filter === "A confirmar" ? "active" : ""}`}
+                onClick={handleFilter}
+              >
+                A confirmar
+              </li>
+              <li
+                className={`${filter === "Facturado" ? "active" : ""}`}
+                onClick={handleFilter}
+              >
+                Facturado
               </li>
               <li
                 className={`${filter === "A cocina" ? "active" : ""}`}
@@ -73,28 +104,30 @@ const Orders = () => {
                 Listo
               </li>
               <li
-                className={`${filter === "En camino" ? "active" : ""}`}
+                className={`${filter === "En delivery" ? "active" : ""}`}
                 onClick={handleFilter}
               >
-                En camino
+                En delivery
               </li>
               <li
-                className={`${filter === "Facturado" ? "active" : ""}`}
+                className={`${filter === "Entregado" ? "active" : ""}`}
                 onClick={handleFilter}
               >
-                Facturado
+                Entregado
               </li>
             </ul>
           </div>
         </div>
-        <form onSubmit={handleSearch}>
-          <input type="text" placeholder="Buscar por cliente" />
-          <button type="submit">
+        <div className="idSearcher__container">
+          <input type="number" placeholder="Buscar por cliente" onChange={(e) => {
+            setIdFilter(Number(e.target.value));
+          }} />
+          <button onClick={handleSearch}>
             <FaSearch />
           </button>
-        </form>
+        </div>
       </div>
-      <Table datos={filterOrders} />
+      <Table datos={filterOrders} setChangeOrderState={setChangeOrderState} />
     </main>
   );
 };
