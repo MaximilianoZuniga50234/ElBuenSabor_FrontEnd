1c94490ad313687f47afb2669b939d041143238a
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2024-01-18 16:02:45 -0300

    Fix categories redirection

--- src/app/components/home/categories/Categories.tsx
+++ src/app/components/home/categories/Categories.tsx
@@ -1,47 +1,98 @@
 import { Link } from "react-router-dom";
+import { useStore as useFilter } from "../../../store/FilterStore";
 import "./categories.css";
 
 const Categories = () => {
+  const { setCategory } = useFilter();
+
   return (
     <div className="categories_main_container">
       <h1>CATEGORIAS</h1>
       <div className="categories_items_container">
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("2");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Pizzas</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264946/assets%20for%20elBuenSabor/pizza-a-la-piedra-1_juxdcg.webp" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("1");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Hamburguesas</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264946/assets%20for%20elBuenSabor/fotografia-de-hamburguesas_btirdn.jpg" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("3");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Lomos</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264946/assets%20for%20elBuenSabor/IS7T6CMJQBEKDKX3IEKNHTWQEU_wunvc5.webp" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("4");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Panchos</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264946/assets%20for%20elBuenSabor/233556-default_ahldt5.avif" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("7");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Minutas</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264946/assets%20for%20elBuenSabor/436282-24a00532-effe-43a2-a025-27d206875cfb_pocuje.jpg" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("6");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Empanadas</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264947/assets%20for%20elBuenSabor/Empanadas-fritas-o-al-horno_akp5we.webp" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("5");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Sándwichs</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264947/assets%20for%20elBuenSabor/maxresdefault_aoypef.jpg" />
         </Link>
-        <Link to="u/products" className="categories_item">
+        <Link
+          to="u/products"
+          onClick={() => {
+            setCategory("8");
+          }}
+          className="categories_item"
+        >
           <div></div>
           <label>Bebidas</label>
           <img src="https://res.cloudinary.com/dycogxoko/image/upload/v1695264947/assets%20for%20elBuenSabor/mejores-bebidas-isotonicas-1640688481_xyacwv.jpg" />
--- src/app/pages/cart/Cart.tsx
+++ src/app/pages/cart/Cart.tsx
@@ -1,19 +1,19 @@
-import "./cart.css";
+import { MouseEvent, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Box, Fade, Modal } from "@mui/material";
+import { FaArrowLeft, FaMinus, FaPlus } from "react-icons/fa6";
 import { useStore } from "../../store/CartStore";
 import { useStore as useCurrentUser } from "../../store/CurrentUserStore";
-import { Link } from "react-router-dom";
-import { CartItem } from "../../interfaces/CartItem";
 import { createPurchaseOrder } from "../../functions/PurchaseOrderAPI";
-import { MouseEvent, useEffect, useState } from "react";
-import { FaArrowLeft, FaMinus, FaPlus } from "react-icons/fa6";
-import { PurchaseOrder } from "../../interfaces/PurchaseOrder";
-import { Person } from "../../interfaces/Person";
 import { getAllAddress } from "../../functions/AddressAPI";
 import { getAllPerson } from "../../functions/PersonAPI";
+import { CartItem } from "../../interfaces/CartItem";
+import { PurchaseOrder } from "../../interfaces/PurchaseOrder";
+import { Person } from "../../interfaces/Person";
 import { Address } from "../../interfaces/Address";
-import { Box, Fade, Modal } from "@mui/material";
-import { useAuth0 } from "@auth0/auth0-react";
 import { PurchaseOrderDetail } from "../../interfaces/PurchaseOrderDetail";
+import "./cart.css";
 
 const Cart = () => {
   const { cartProducts, remove, clear, removeOne, addOne } = useStore();
@@ -58,12 +58,12 @@ const Cart = () => {
     if (isAuthenticated) getToken();
   }, [isAuthenticated]);
 
-  const { user } = useCurrentUser()
+  const { user } = useCurrentUser();
 
   const [priceAndTime, setPriceAndTime] = useState({
     totalPrice: 0,
-    totalTime: 0
-  })
+    totalTime: 0,
+  });
 
   const [purchaseOrder, setPurchaseOrder] = useState<PurchaseOrder>({
     id: 0,
@@ -73,17 +73,31 @@ const Cart = () => {
     shippingType: "Retiro en el local",
     paymentMethod: "Efectivo",
     total: 0,
-    person: { id: "0", name: "", email: "", lastName: "", phoneNumber: "", user_id: "" },
+    person: {
+      id: "0",
+      name: "",
+      email: "",
+      lastName: "",
+      phoneNumber: "",
+      user_id: "",
+    },
     address: {
       id: 0,
       street: "",
       number: 0,
       department: { id: 0, name: "" },
-      person: { id: "0", name: "", email: "", lastName: "", phoneNumber: "", user_id: "" }
+      person: {
+        id: "0",
+        name: "",
+        email: "",
+        lastName: "",
+        phoneNumber: "",
+        user_id: "",
+      },
     },
     status: { id: 1, status: "Por aceptar" },
     details: null,
-  })
+  });
 
   useEffect(() => {
     if (purchaseOrder.details && purchaseOrder.details?.length > 0) {
@@ -92,154 +106,171 @@ const Cart = () => {
   }, [purchaseOrder.details]);
 
   useEffect(() => {
-    if (user?.user_id != "" &&
+    if (
+      user?.user_id != "" &&
       personsDatabase &&
       personsDatabase.length > 0 &&
       addressesDatabase &&
-      addressesDatabase?.length > 0) {
+      addressesDatabase?.length > 0
+    ) {
+      const person = personsDatabase.find(
+        (person) => person.user_id === user?.user_id
+      );
+      const address = addressesDatabase?.find(
+        (address) => address.person.id === person?.id
+      );
 
-      const person = personsDatabase.find((person) => person.user_id === user?.user_id)
-      const address = addressesDatabase?.find((address) => address.person.id === person?.id)
-
-      setPurchaseOrder({ ...purchaseOrder, address: address, person: person })
+      setPurchaseOrder({ ...purchaseOrder, address: address, person: person });
     }
-  }, [user, personsDatabase, addressesDatabase])
+  }, [user, personsDatabase, addressesDatabase]);
 
   useEffect(() => {
     if (cartProducts.length > 0) {
-      let time = 0
-      let price = 0
+      let time = 0;
+      let price = 0;
       cartProducts.map(async (cartProduct) => {
-        time = time + cartProduct.product.estimatedTimeKitchen
-        price = price + cartProduct.product.salePrice * cartProduct.amount
-      })
-      setPriceAndTime({ totalPrice: price, totalTime: time })
+        time = time + cartProduct.product.estimatedTimeKitchen;
+        price = price + cartProduct.product.salePrice * cartProduct.amount;
+      });
+      setPriceAndTime({ totalPrice: price, totalTime: time });
     }
-
-  }, [cartProducts])
+  }, [cartProducts]);
 
   useEffect(() => {
-    setPurchaseOrder({ ...purchaseOrder, estimatedEndTime: priceAndTime.totalTime, total: priceAndTime.totalPrice })
-  }, [priceAndTime])
+    setPurchaseOrder({
+      ...purchaseOrder,
+      estimatedEndTime: priceAndTime.totalTime,
+      total: priceAndTime.totalPrice,
+    });
+  }, [priceAndTime]);
 
-  const handleChangeShippingType = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setPurchaseOrder({ ...purchaseOrder, shippingType: event.target.value, paymentMethod: event.target.value === "Retiro en el local" ? "Efectivo" : "Mercado Pago" })
+  const handleChangeShippingType = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setPurchaseOrder({
+      ...purchaseOrder,
+      shippingType: event.target.value,
+      paymentMethod:
+        event.target.value === "Retiro en el local"
+          ? "Efectivo"
+          : "Mercado Pago",
+    });
   };
 
   const handleConfirm = async (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    const details: PurchaseOrderDetail[] = cartProducts.map(cartProduct => {
+    const details: PurchaseOrderDetail[] = cartProducts.map((cartProduct) => {
       return {
         amount: cartProduct.amount,
         subtotal: cartProduct.product.salePrice * cartProduct.amount,
         product: cartProduct.product,
-        stock: null
-      } as PurchaseOrderDetail
-    })
-    setPurchaseOrder({ ...purchaseOrder, details: details })
-    handleClose()
+        stock: null,
+      } as PurchaseOrderDetail;
+    });
+    setPurchaseOrder({ ...purchaseOrder, details: details });
+    handleClose();
     clear();
   };
 
-  return (
-
-    isAuthenticated ?
-      <>
-        < div className="cart_main_container" >
-          <h2>Carrito de compras</h2>
-          <div className="cart_back_amount">
-            <Link to="/">
-              <FaArrowLeft />
-              Volver
-            </Link>
-            {cartProducts.length > 0 ? (
-              <p>
-                <b>{cartProducts.length}</b> Productos
-              </p>
-            ) : (
-              <p>Sin productos</p>
-            )}
-          </div>
-          <div className="cart_items_container">
-            {cartProducts.map((cartItem: CartItem) => (
-              <div key={cartItem.product.id} className="cart_item">
-                <img src={cartItem.product.imgUrl} />
-                <div>
-                  <h3>{cartItem.product.denomination}</h3>
-                  <p>Precio: ${cartItem.product.salePrice * cartItem.amount}</p>
-                </div>
-                <button onClick={() => remove(cartItem.product.id)}>
-                  Eliminar
-                </button>
-                <div>
-                  {cartItem.amount > 1 && (
-                    <FaMinus onClick={() => removeOne(cartItem.product.id)} />
-                  )}
-                  <input
-                    type="number"
-                    className="cart_item_amount_input"
-                    value={cartItem.amount}
-                    readOnly
-                  />
-                  {cartItem.amount < 99 && (
-                    <FaPlus onClick={() => addOne(cartItem.product.id)} />
-                  )}
-                  {cartItem.amount > 1 ? <p>Uds.</p> : <p>Ud.</p>}
-                </div>
+  return isAuthenticated ? (
+    <>
+      <div className="cart_main_container">
+        <h2>Carrito de compras</h2>
+        <div className="cart_back_amount">
+          <Link to="/">
+            <FaArrowLeft />
+            Volver
+          </Link>
+          {cartProducts.length > 0 ? (
+            <p>
+              <b>{cartProducts.length}</b> Productos
+            </p>
+          ) : (
+            <p>Sin productos</p>
+          )}
+        </div>
+        <div className="cart_items_container">
+          {cartProducts.map((cartItem: CartItem) => (
+            <div key={cartItem.product.id} className="cart_item">
+              <img src={cartItem.product.imgUrl} />
+              <div>
+                <h3>{cartItem.product.denomination}</h3>
+                <p>Precio: ${cartItem.product.salePrice * cartItem.amount}</p>
               </div>
-            ))}
-          </div>
-          <div className="cart_buttons_container">
-            <button onClick={() => clear()}>Limpiar</button>
-            <button onClick={handleOpen}>Elegir forma de entrega</button>
-          </div>
-        </div >
-        <Modal
-          open={open}
-          onClose={handleClose}
-          slotProps={{
-            backdrop: {
-              timeout: 300,
-            },
-          }}
-          disableScrollLock={true}
-        >
-          <Fade in={open}>
-            <Box className='modalCart__box'>
-              <h3 className="modalCart__h3">
-                Elegir forma de envío
-              </h3>
-
-              <div className="modalCart__div">
-                <h5 className="modalCart__h5">Forma de entrega</h5>
-                <select
-                  className="modalCart__select"
-                  onChange={handleChangeShippingType}
-                  defaultValue={purchaseOrder.shippingType}
-                  placeholder="Ingrese el departamento del cliente"
-                >
-                  <option value="Retiro en el local">Retiro en el local</option>
-                  <option value="Envío a domicilio">Envío a domicilio</option>
-                </select>
+              <button onClick={() => remove(cartItem.product.id)}>
+                Eliminar
+              </button>
+              <div>
+                {cartItem.amount > 1 && (
+                  <FaMinus onClick={() => removeOne(cartItem.product.id)} />
+                )}
+                <input
+                  type="number"
+                  className="cart_item_amount_input"
+                  value={cartItem.amount}
+                  readOnly
+                />
+                {cartItem.amount < 99 && (
+                  <FaPlus onClick={() => addOne(cartItem.product.id)} />
+                )}
+                {cartItem.amount > 1 ? <p>Uds.</p> : <p>Ud.</p>}
               </div>
+            </div>
+          ))}
+        </div>
+        <div className="cart_buttons_container">
+          <button onClick={() => clear()}>Limpiar</button>
+          <button onClick={handleOpen}>Elegir forma de entrega</button>
+        </div>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          backdrop: {
+            timeout: 300,
+          },
+        }}
+        disableScrollLock={true}
+      >
+        <Fade in={open}>
+          <Box className="modalCart__box">
+            <h3 className="modalCart__h3">Elegir forma de envío</h3>
 
-              <div className="modalCart__buttons">
-                <button className="modalCart__button" onClick={() => { handleClose() }}>Cancelar</button>
-                <button className="modalCart__button"
-                  onClick={handleConfirm} >
-                  Confirmar
-                </button>
-              </div>
-            </Box>
-          </Fade>
-        </Modal >
-      </>
+            <div className="modalCart__div">
+              <h5 className="modalCart__h5">Forma de entrega</h5>
+              <select
+                className="modalCart__select"
+                onChange={handleChangeShippingType}
+                defaultValue={purchaseOrder.shippingType}
+                placeholder="Ingrese el departamento del cliente"
+              >
+                <option value="Retiro en el local">Retiro en el local</option>
+                <option value="Envío a domicilio">Envío a domicilio</option>
+              </select>
+            </div>
 
-      :
-
-      <div className="div__noSesion">
-        <h1>Debes iniciar sesión para ver el carrito</h1>
-      </div >
+            <div className="modalCart__buttons">
+              <button
+                className="modalCart__button"
+                onClick={() => {
+                  handleClose();
+                }}
+              >
+                Cancelar
+              </button>
+              <button className="modalCart__button" onClick={handleConfirm}>
+                Confirmar
+              </button>
+            </div>
+          </Box>
+        </Fade>
+      </Modal>
+    </>
+  ) : (
+    <div className="div__noSesion">
+      <h1>Debes iniciar sesión para ver el carrito</h1>
+    </div>
   );
 };
 
