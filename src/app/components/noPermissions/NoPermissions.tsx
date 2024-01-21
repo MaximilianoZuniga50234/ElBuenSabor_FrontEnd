58b07096e9dbb851382913f85096634a9bef2126
Lautaro Jofré <lautyjofre1@gmail.com>
2024-01-21 13:06:14 -0300

    Creo componente para mostrar cuando el usuario no tiene permisos de acceso

---
+++ src/app/components/noPermissions/NoPermissions.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import "./noPermissions.css"
+
+
+export default function NoPermissions() {
+    return (
+        <div className="noPermissions__container">
+            <div className="noPermissions__elements">
+                <h1 className="noPermissions__h1">
+                    Ups! Parece que no tienes permisos para acceder a esta sección.
+                </h1>
+                <button className="noPermissions__button">
+                    <Link to={"/"}>
+                        Volver al inicio
+                    </Link>
+                </button>
+            </div>
+        </div>
+    )
+}
